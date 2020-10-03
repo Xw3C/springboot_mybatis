6e76c83c5dd65e3700ede3f3bc7dbed5c5bc8b44
Xw Chen <54886844+Xw3C@users.noreply.github.com>
2020-10-03 18:00:39 +0800

    Initial commit

---
+++ src/main/java/com/it/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.it.mapper;
+
+import com.it.domain.User;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+    public List<User> queryUserList();
+}
